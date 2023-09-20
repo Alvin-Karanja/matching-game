0312e9c1c06aef266f89cea3680968037ee5ea4b
Alvin-Karanja <alvin2@proton.me>
2023-09-20 13:20:49 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import OptionsPanel from "../OptionsPanel";
+import Board from "../Board";
+
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Turbo-Matcher</header>
+        <OptionsPanel
+          playing={this.state.playing}
+          numTiles={this.state.numTiles}
+        />
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+      </div>
+    );
+  }
+}
+
+export default App;
