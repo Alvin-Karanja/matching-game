19b00df094f864e797d5fde4ad7e8108925d0353
Alvin-Karanja <alvin2@proton.me>
2023-09-20 13:58:37 +0300

    create a custom hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import OptionsPanel from "../OptionsPanel";
-import Board from "../Board";
+import OptionsPanel from "../components/OptionsPanel";
+import Board from "../components/Board";
 
 import "./App.css";
+import { createTiles } from "../../misc/utils";
 
 class App extends Component {
   constructor(props) {
@@ -24,10 +25,28 @@ class App extends Component {
         <OptionsPanel
           playing={this.state.playing}
           numTiles={this.state.numTiles}
+          startGame={this.startGame}
         />
-        <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+        <Board
+          numTiles={this.state.numTiles}
+          tiles={this.state.tiles}
+          handleTileClicked={this.handleTileClicked}
+        />
       </div>
     );
+  }
+
+  handleTileClicked(id, color) {
+    // TODO: Implement the handleTileClicked() method
+  }
+
+  startGame(numTiles) {
+    this.setState(() => ({
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(numTiles),
+    }));
   }
 }
 
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import "./Tile.css";
 
-import './Tile.css'
-
-const Tile = () => {
+const Tile = ({ selected, matched, color, svg }) => {
+  const dynamicColor = selected || matched ? { backgroundColor: color } : null;
 
   return (
-    <div className='Tile'>
+    <div className="Tile" style={dynamicColor}>
+      {selected || matched ? <svg>{svg}</svg> : null}
     </div>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
