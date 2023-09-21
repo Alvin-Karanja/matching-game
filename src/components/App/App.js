7e99e08580363c6c3097276482bbb9bb08d31a05
Alvin-Karanja <alvin2@proton.me>
2023-09-21 10:08:58 +0300

    Handling click events

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import OptionsPanel from "../components/OptionsPanel";
 import Board from "../components/Board";
 
 import "./App.css";
-import { createTiles } from "../../misc/utils";
+import { createTiles, indexOfSelected } from "../../misc/utils";
 
 class App extends Component {
   constructor(props) {
@@ -37,7 +37,46 @@ class App extends Component {
   }
 
   handleTileClicked(id, color) {
-    // TODO: Implement the handleTileClicked() method
+    this.setState((state) => {
+      const tiles = state.tiles;
+      const toBeCleared = tiles[id];
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+      const previousTileIndex = state.previousTileIndex;
+
+      // If there are tiles to be cleared, clear them.
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
+      }
+
+      // If there is a previous tile index, check if it matches the current tile.
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex];
+        const selectedTile = tiles[selectedTileIndex];
+
+        if (
+          previousTile.id !== selectedTile.id &&
+          previousTile.color === color
+        ) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      }
+
+      // Set the selected property of the current tile to true.
+      tiles[selectedTileIndex].selected = true;
+
+      return {
+        toBeCleared,
+        tiles,
+        previousTileIndex,
+      };
+    });
   }
 
   startGame(numTiles) {
@@ -45,7 +84,7 @@ class App extends Component {
       playing: true,
       previousTileIndex: null,
       toBeCleared: null,
-      tiles: createTiles(numTiles),
+      tiles: createTiles(numTiles, this.handleTileClicked), // Pass this.handleTileClicked as an argument
     }));
   }
 }
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,10 +1,15 @@
+import React from "react";
 import "./Tile.css";
 
-const Tile = ({ selected, matched, color, svg }) => {
+const Tile = ({ selected, matched, color, svg, handleTileClicked }) => {
   const dynamicColor = selected || matched ? { backgroundColor: color } : null;
 
   return (
-    <div className="Tile" style={dynamicColor}>
+    <div
+      className="Tile"
+      style={dynamicColor}
+      onClick={() => handleTileClicked(props.id, props.color)}
+    >
       {selected || matched ? <svg>{svg}</svg> : null}
     </div>
   );
